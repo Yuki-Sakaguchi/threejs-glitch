22fd980552a95fbbb03b506e0d88c8787720d865
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2021-08-15 21:05:39 +0900

    mod part03

--- part03/next.config.js
+++ part03/next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV == 'production';
 
 module.exports = {
   basePath: isProd ? '/threejs-glitch/part03/out' : '',
-  assetPrefix:  isProd ? 'https://yuki-sakaguchi.github.io/threejs-glitch/part03/out' : '',
+  assetPrefix: isProd ? 'https://yuki-sakaguchi.github.io/threejs-glitch/part03/out' : '',
   webpack: (config) => {
     config.module.rules.push(
       {
--- part03/pages/index.tsx
+++ part03/pages/index.tsx
@@ -2,10 +2,10 @@ import Glitch from '@/components/Glitch';
 
 export default function Home() {
   const images = [
-    '/images/img01.png',
-    '/images/img02.png',
-    '/images/img03.png',
-    '/images/img04.png',
+    'images/img01.png',
+    'images/img02.png',
+    'images/img03.png',
+    'images/img04.png',
   ];
   return (
     <>
